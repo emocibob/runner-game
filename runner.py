9667738d7762d8f94b8763e669f5baf1f8646b6e
Edvin Mocibob <edvin.mocibob@gmail.com>
2022-02-16 20:46:01 +0100

    Add player and use rectangles

--- runner.py
+++ runner.py
@@ -13,7 +13,10 @@ ground_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("My game", False, "Black").convert()
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(bottomright=(600, 300))
+
+player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80, 300))
 
 while True:
     for event in pygame.event.get():
@@ -24,12 +27,13 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
-    snail_x_pos -= 4
+    snail_rect.x -= 4
 
-    if snail_x_pos < -100:
-        snail_x_pos = 800
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
 
-    screen.blit(snail_surface, (snail_x_pos, 250))
+    screen.blit(snail_surface, snail_rect)
+    screen.blit(player_surface, player_rect)
 
     pygame.display.update()
     clock.tick(60)
