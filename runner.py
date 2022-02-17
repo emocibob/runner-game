5d2f2137f5c3c07f9b16419f0e5b7bdb7097ac5b
Edvin Mocibob <edvin.mocibob@gmail.com>
2022-02-17 20:54:35 +0100

    Calculate and display score

--- runner.py
+++ runner.py
@@ -2,23 +2,24 @@ from sys import exit
 
 import pygame
 
+
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surface = test_font.render(f"Score: {current_time}", False, (64, 64, 64))
+    score_rect = score_surface.get_rect(center=(400, 50))
+    screen.blit(score_surface, score_rect)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 50)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground_surface = pygame.image.load("graphics/ground.png").convert()
-
-score_surface = test_font.render("My game", False, (64, 64, 64))
-score_rect = score_surface.get_rect(center=(400, 50))
-
-score_background_rect = pygame.Rect(
-    score_rect[0], score_rect[1], score_rect[2] + 16, score_rect[3] + 16
-)
-score_background_rect.center = (400, 50)
 
 snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright=(600, 300))
@@ -44,12 +45,12 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = int(pygame.time.get_ticks() / 1000)
 
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, "#c0e8ec", score_background_rect, border_radius=10)
-        screen.blit(score_surface, score_rect)
+        display_score()
         snail_rect.x -= 4
 
         if snail_rect.right <= 0:
